70f7dd5456975f674b4fc0bc6f135b0659d544e8
Rajat Gupta <rajatgupta0916@gmail.com>
2020-10-25 01:45:50 +0530

    Updates

---
+++ precache-manifest.4680c41c68cd2de15ae4a017ac870f1e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e11720856ed2754f3f043d81c88200b9",
+    "url": "/ProductDetails/index.html"
+  },
+  {
+    "revision": "f3612dc221d57679b329",
+    "url": "/ProductDetails/static/css/2.44309f2b.chunk.css"
+  },
+  {
+    "revision": "7c5358170d5cbe20f3e0",
+    "url": "/ProductDetails/static/css/main.621ad97f.chunk.css"
+  },
+  {
+    "revision": "f3612dc221d57679b329",
+    "url": "/ProductDetails/static/js/2.97f2bf36.chunk.js"
+  },
+  {
+    "revision": "d85b4c5fd0a6251ed5a81543d734b2bc",
+    "url": "/ProductDetails/static/js/2.97f2bf36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c5358170d5cbe20f3e0",
+    "url": "/ProductDetails/static/js/main.eb76aa4a.chunk.js"
+  },
+  {
+    "revision": "cca6415e45c25bdee8d3",
+    "url": "/ProductDetails/static/js/runtime-main.67186b46.js"
+  }
+]);
